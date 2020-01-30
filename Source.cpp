c749743e4b5ff6226c8f2397c683db0fba125f01
gerson <gersongames2017@gmail.com>
2020-01-30 09:12:05 -0800

    primer commit

---
+++ Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int matriz[15][10], filas,i, j, total_devegado = 0;
+
+int main()
+{
+	cout << "ingrese cantidad de empleados" << endl;
+	cin >> filas;
+	for (i = 0; i < 15; i++)
+	{
+
+	}
+	system("pause");
+	return 0;
+}
